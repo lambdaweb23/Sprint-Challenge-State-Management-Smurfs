b7cda795a04abf9aa28f3f282b21b362ac85d1d3
Kenna <kennaalastair@gmail.com>
2019-10-11 10:28:04 -0700

    displaying smurfs

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -2,17 +2,21 @@ import React, { Component } from "react";
 
 import { getSmurfs } from '../actions';
 import { connect } from 'react-redux';
-import AddSmurf from './AddSmurf';
+
 import "./App.css";
+import AddSmurf from './AddSmurf';
+import SmurfList from "./SmurfList";
 class App extends Component {
   componentDidMount() {
     this.props.getSmurfs();
   }
+
   render() {
     return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
         <AddSmurf />
+        <SmurfList />
       </div>
     );
   }
